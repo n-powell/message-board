9c2caa984cfc1d5776db69e35501ba267ce41e1b
Nick Powell <nickpowell@Nicks-iMac.local>
2017-06-02 11:58:46 -0700

    add question update

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        user: this.get('user'),
+        title: this.get('title'),
+        body: this.get('body'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
+  }
 });
